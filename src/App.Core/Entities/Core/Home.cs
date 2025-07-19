2ffc469f16a3f57f4b83a8f000ce81e30504d16a
hsnvagil <hsnvagil@gmail.com>
2025-07-20 03:41:52 +0400

    refactor home entity

--- src/App.Core/Entities/Core/Home.cs
+++ src/App.Core/Entities/Core/Home.cs
@@ -3,7 +3,6 @@
 public class Home {
     public long Id { get; set; }
     public string Name { get; set; }
-    public List<DateOnly> AvailableSlots { get; set; }
     
     public override bool Equals(object? obj) =>
         obj is Home other && Id == other.Id;
